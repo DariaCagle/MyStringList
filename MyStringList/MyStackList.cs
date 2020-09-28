3c2ead1016cd473b28977ac239a01a6ef6f6ad96
DariaCagle <dani.merchard@gmail.com>
2020-09-28 11:48:44 +0300

    separate classes

---
+++ MyStringList/MyStackList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+
+namespace MyStringList
+{
+    public class MyStackList : IEnumerable
+    {
+        Node head;
+        int count;
+
+        public object this[int index]
+        {
+            get
+            {
+                if (index >= Count)
+                    throw new IndexOutOfRangeException("Unknown index");
+                int i = 0;
+                foreach (var item in this)
+                {
+                    if (i == index) return item;
+                    i++;
+                }
+                return this[index];
+            }
+        }
+
+        public bool IsEmpty
+        {
+            get { return count == 0; }
+        }
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public void Push(object item)
+        {
+            Node node = new Node(item);
+            node.Next = head;
+            head = node;
+            count++;
+        }
+        public object Pop()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Stack is emplty");
+            Node previous = head;
+            head = head.Next;
+            count--;
+            return previous.Data;
+        }
+        public object Peek()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Stack is empty");
+            return head.Data;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}
---
+++ MyStringList/Node.cs
@@ -0,0 +1,13 @@
+﻿namespace MyStringList
+{
+    public class Node
+    {
+        public Node(object data)
+        {
+            Data = data;
+        }
+        public object Data { get; set; }
+
+        public Node Next { get; set; }
+    }
+}
